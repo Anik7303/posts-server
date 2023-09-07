6acea24ca3e745cc049c16f49f5d32d8c5cf1785
Anik Mohammad <anik7303@gmail.com>
2023-09-07 20:09:27 +0600

    refactor: src/index.ts, package.json

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,10 @@ import "./models/user";
 // middlewares
 import { error404Middleware, errorsMiddleware } from "./middlewares";
 
+// other imports
+import { getNetworkIP } from "./lib/utils";
+
 // environment variables
-const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || "8000";
 const DB_URI = process.env.DB_URI || "mongodb://localhost:27017/test";
 
@@ -32,8 +34,9 @@ app.use(errorsMiddleware);
 
 async function main(): Promise<void> {
   try {
-    app.listen(parseInt(PORT), HOST, (): void => {
-      console.log(`Server address http://${HOST}:${PORT}`);
+    app.listen(parseInt(PORT), (): void => {
+      console.log(`Local address: http://127.0.0.1:${PORT}`);
+      console.log(`Network address: http://${getNetworkIP()}:${PORT}`);
     });
     await mongoose.connect(DB_URI, {
       serverApi: {
